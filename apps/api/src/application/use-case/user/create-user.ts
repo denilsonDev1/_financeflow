2ad80ca19671b4841e298101337117fb68a5b99f
Denilson Oliveira <juniod04@gmail.com>
2025-09-09 17:13:48 -0300

    fix: move createUserUseCase for folder user in use-case

--- apps/api/src/application/use-case/user/create-user.ts
+++ apps/api/src/application/use-case/user/create-user.ts
@@ -1,6 +1,6 @@
-import { User as UserEntity } from "../../domain/user/user.entity";
-import type { UserRepository } from "../../infrastructure/database/repositories/user";
-import type { User } from "../../presentation/http/controllers/user/dto";
+import { User as UserEntity } from "../../../domain/user/user.entity";
+import type { UserRepository } from "../../../infrastructure/database/repositories/user";
+import type { User } from "../../../presentation/http/controllers/user/dto";
 
 export class CreateUserUseCase{
     constructor(private userRepository: UserRepository){}

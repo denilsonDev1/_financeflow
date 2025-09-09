4d83d9a985876b968f058cef36a2e4f9860390e2
Denilson Oliveira <juniod04@gmail.com>
2025-09-09 17:21:15 -0300

    feat: add index in controller of user

--- apps/api/src/presentation/http/controllers/user/index.ts
+++ apps/api/src/presentation/http/controllers/user/index.ts
@@ -1,9 +1,10 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
-import { CreateUserUseCase } from "../../../../application/use-case/create-user";
+import { CreateUserUseCase } from "../../../../application/use-case/user/create-user";
+import type { FindUserByIdUseCase } from "../../../../application/use-case/user/find-user-by-id";
 import type { User } from "./dto";
 
 export class UserController {
-    constructor(private createUserUseCase: CreateUserUseCase){
+    constructor(private createUserUseCase: CreateUserUseCase, private findUserByIdUseCase: FindUserByIdUseCase){
     }
     public async store(req: FastifyRequest<{Body: User}>, res: FastifyReply){
         try{
@@ -15,5 +16,16 @@ export class UserController {
             return res.code(500).send({error: "internal server error"})
         
         }
+    }
+
+    public async index(req: FastifyRequest<{Params: {id: string}}>, res: FastifyReply){
+        const users = await this.findUserByIdUseCase.execute(req.params.id)
+        if(!users.success) return res.code(500).send({error: "server internal error"})
+        
+        return res.code(200).send({
+            data: users.data ?? {}
+        
+        })
+    
     }
 }

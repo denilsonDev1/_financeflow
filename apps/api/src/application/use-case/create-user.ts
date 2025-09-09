d43531b839a0972e0e47e62b1e3abb3905f1fc82
Denilson Oliveira <juniod04@gmail.com>
2025-09-09 13:13:37 -0300

    feat: add createUserUseCase

---
+++ apps/api/src/application/use-case/create-user.ts
@@ -0,0 +1,14 @@
+import { User as UserEntity } from "../../domain/user/user.entity";
+import type { UserRepository } from "../../infrastructure/database/repositories/user";
+import type { User } from "../../presentation/http/controllers/user/dto";
+
+export class CreateUserUseCase{
+    constructor(private userRepository: UserRepository){}
+
+    async execute(data: User){
+        const entity = UserEntity.create(data).getData()
+        const user = await this.userRepository.create(entity)
+        return user
+    }
+}
+

407c8431804105c122eaa5cc3b43e0d613ee97e5
Denilson Oliveira <juniod04@gmail.com>
2025-09-09 17:21:36 -0300

    feat: add router for get user by id

--- apps/api/src/presentation/http/routes/user.ts
+++ apps/api/src/presentation/http/routes/user.ts
@@ -1,11 +1,14 @@
 import type { FastifyInstance } from "fastify";
-import { CreateUserUseCase } from "../../../application/use-case/create-user";
+import { z } from "zod";
+import { CreateUserUseCase } from "../../../application/use-case/user/create-user";
+import { FindUserByIdUseCase } from "../../../application/use-case/user/find-user-by-id";
 import { UserRepository } from "../../../infrastructure/database/repositories/user";
 import { UserController } from "../controllers/user";
 import { userBodySchema } from "../controllers/user/dto";
 const userRepository = new UserRepository()
     const createUserUseCase = new CreateUserUseCase(userRepository)
-    const userController = new UserController(createUserUseCase)
+    const findUserByIdUseCase = new FindUserByIdUseCase(userRepository)
+    const userController = new UserController(createUserUseCase, findUserByIdUseCase)
 export const userRouter = (app: FastifyInstance) => {
     
     app.post("/", {
@@ -13,4 +16,13 @@ export const userRouter = (app: FastifyInstance) => {
             body: userBodySchema
         }
     },userController.store.bind(userController))
+
+    app.get("/:id", {
+        schema: {
+            params: z.object({
+                id: z.cuid()
+            })
+        }
+    }, userController.index.bind(userController))
+
 }

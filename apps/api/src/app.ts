3190f791d814352cf6202a1ed5f18e371d93130d
Denilson Oliveira <juniod04@gmail.com>
2025-09-10 16:54:00 -0300

    fix: refactor app

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,16 +1,31 @@
-import fastifySwagger from "@fastify/swagger"
-import { fastify } from "fastify"
-import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
-import { erroHandler } from "./infrastructure/handlers/errorHandler"
-import { router } from "./presentation/http/routes"
-
-export const app = fastify({logger: true})
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
+import fastifySwagger from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastify } from "fastify";
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
+import { erroHandler } from "./infrastructure/handlers/errorHandler";
+import { router } from "./presentation/http/routes";
+export const app = fastify({ logger: true });
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifySwagger, {
-    mode: "dynamic",
-    transform: jsonSchemaTransform,
-})
-app.setErrorHandler(erroHandler)
-app.register(router)
+	openapi: {
+		info: {
+			title: "FinanceFlow",
+			description: "",
+			version: "1.0.0",
+		},
+	},
+
+	mode: "dynamic",
+	transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUi, {
+	routePrefix: "/docs",
+});
+app.setErrorHandler(erroHandler);
+app.register(router);

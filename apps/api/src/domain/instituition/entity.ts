669a6619d142b79b4e8668e8484450a3211fe8fc
Denilson Oliveira <juniod04@gmail.com>
2025-09-10 16:53:15 -0300

    feat: add entity for instituition

---
+++ apps/api/src/domain/instituition/entity.ts
@@ -0,0 +1,35 @@
+import cuid from "cuid";
+import type { InstituitionData, InstitutionType } from "./types";
+
+export class Instituition {
+	private readonly id: string;
+	private name: string;
+	shortName: string;
+	private logoUrl?: string;
+	type: InstitutionType;
+	private primaryColor?: string;
+
+	private constructor(data: InstituitionData) {
+		this.id = cuid();
+		this.name = data.name;
+		this.shortName = data.shortName;
+		this.logoUrl = data.logoUrl;
+		this.type = data.type;
+		this.primaryColor = data.primaryColor;
+	}
+
+	public static create(data: InstituitionData) {
+		return new Instituition(data);
+	}
+
+	public getData() {
+		return {
+			id: this.id,
+			name: this.name,
+			shortName: this.shortName,
+			logoUrl: this.logoUrl,
+			type: this.type,
+			primaryColor: this.primaryColor,
+		};
+	}
+}
